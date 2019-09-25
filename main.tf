0ae5998f587f3d61e4b4f50ac03426662a8ce690
Loren Gordon <lorengordon@users.noreply.github.com>
2019-09-25 06:49:54 -0700

    Provides a terraform module to manage the lambda function

--- main.tf
+++ main.tf
@@ -1,1 +1,91 @@
+terraform {
+  required_version = ">= 0.12"
+}
 
+provider "aws" {
+}
+
+locals {
+  name = "new-account-trust-policy-${random_string.id.result}"
+}
+
+data "aws_partition" "current" {
+}
+
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    actions = [
+      "organizations:DescribeCreateAccountStatus"
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    resources = [
+      "arn:${data.aws_partition.current.partition}:iam::*:role/${var.assume_role_name}",
+    ]
+  }
+}
+
+resource "random_string" "id" {
+  length  = 13
+  special = false
+}
+
+module "lambda" {
+  source = "git::https://github.com/plus3it/terraform-aws-lambda.git?ref=v1.1.0"
+
+  function_name = local.name
+  description   = "Post messages from AWS to Slack"
+  handler       = "new-account-trust-policy.lambda_handler"
+  policy        = data.aws_iam_policy_document.lambda
+  runtime       = "python3.6"
+  source_path   = "${path.module}/new-account-trust-policy.py"
+  timeout       = 300
+
+  environment = {
+    variables = {
+      ASSUME_ROLE_NAME = var.assume_role_name
+      UPDATE_ROLE_NAME = var.update_role_name
+      TRUST_POLICY     = var.trust_policy
+      LOG_LEVEL        = var.log_level
+    }
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "this" {
+  name          = local.name
+  description   = "Managed by Terraform"
+  event_pattern = <<-PATTERN
+    {
+      "source": ["aws.organizations"],
+      "detail-type": ["AWS API Call via CloudTrail"],
+      "detail": {
+        "eventSource": ["organizations.amazonaws.com"],
+        "eventName": [
+            "InviteAccountToOrganization",
+            "CreateAccount"
+        ]
+      }
+    }
+    PATTERN
+}
+
+resource "aws_cloudwatch_event_target" "this" {
+  rule = aws_cloudwatch_event_rule.this.name
+  arn  = module.lambda.function_arn
+}
+
+resource "aws_lambda_permission" "events" {
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.this.arn
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "assume_role_name" {
+  description = "Name of the IAM role to assume in the target account (case sensitive)"
+  type        = string
+}
 
+variable "update_role_name" {
+  description = "Name of the IAM role to update in the target account (case sensitive)"
+  type        = string
+}
+
+variable "trust_policy" {
+  description = "JSON string representing the trust policy to apply to the role being updated"
+  type        = string
+}
+
+variable "log_level" {
+  default     = "Info"
+  description = "Log level of the lambda output, one of: Debug, Info, Warning, Error, Critical"
+  type        = string
+}
