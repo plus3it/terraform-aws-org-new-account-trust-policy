19fe2208f9b112f82b62ea171f457a2cc9d341d9
Loren Gordon <lorengordon@users.noreply.github.com>
2022-11-14 12:32:24 -0800

    Creates simple terraform test configs to exercise the module

---
+++ tests/test_create_package_separately/main.tf
@@ -0,0 +1,24 @@
+module "test_create_package" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-lambda.git?ref=v4.7.1"
+
+  create_function = false
+  create_package  = true
+
+  recreate_missing_package = false
+
+  runtime     = "python3.8"
+  source_path = "${path.module}/../../lambda/src"
+}
+
+module "test_create_function" {
+  source = "../.."
+
+  assume_role_name = "FOO"
+  update_role_name = "BAR"
+  trust_policy     = jsonencode({})
+
+  lambda = {
+    local_existing_package = "${path.module}/${module.test_create_package.local_filename}"
+    create_package         = false
+  }
+}
